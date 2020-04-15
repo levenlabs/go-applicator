c27aad611cc7a27dec2c89c6b66b26a11b1a371b
James Hartig <james@getadmiral.com>
2020-04-15 11:35:25 -0400

    Return ErrCannotApply from Apply instead of ErrUnsupported

--- errors.go
+++ errors.go
@@ -4,8 +4,16 @@ import (
 	"errors"
 )
 
+// ErrUnsupported is returned from an applicator that received an incorrect type.
+// Like the trim receiving an integer.
 var ErrUnsupported = errors.New("unsupported type encountered")
 
+// ErrNotFound is returned when the tag specified an unknown applicator.
 var ErrNotFound = errors.New("applicator name not found")
 
+// ErrInvalidSet is returned when the applicator returned a value that doesn't
+// match the field type.
 var ErrInvalidSet = errors.New("cannot set new value with unmatching type")
+
+// ErrCannotApply is returned when you call Apply on a non-struct pointer.
+var ErrCannotApply = errors.New("cannot apply to invalid type")
